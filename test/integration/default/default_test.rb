03bb8e2ec354c78d16f36c633d977f90068c989a
Ahmad Wilson <ahwilson712@gmail.com>
2019-10-21 21:10:42 -0400

    update common configuration and verification steps

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,3 +5,19 @@ describe service('sshd') do
   it { should be_enabled }
   it { should be_running }
 end
+
+describe file('/etc/ssh/sshd_config.bak') do
+  it { should exist }
+  its('owner') { should eq 'root' }
+  its('group') { should eq 'root' }
+  its('mode') { should cmp '0644' }
+end
+
+describe file('/etc/ssh/sshd_config') do
+  it { should exist }
+  its('owner') { should eq 'root' }
+  its('group') { should eq 'root' }
+  its('mode') { should cmp '0600' }
+  its('content') { should match("Managed by Ansible") }
+  its('content') { should match("ansible-role-openssh") }
+end
