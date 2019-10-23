a618509879e57cd1827fe7e6243b3c477c3595d5
Ahmad Wilson <ahwilson712@gmail.com>
2019-10-23 16:51:14 -0400

    add automatic ssh-agent launch and key management functionality

--- test/integration/service_config/default_test.rb
+++ test/integration/service_config/default_test.rb
@@ -1,11 +1,5 @@
 title "Default role integrated test file"
 
-describe service('sshd') do
-  it { should be_installed }
-  it { should be_enabled }
-  it { should be_running }
-end
-
 describe file('/etc/ssh/sshd_config') do
   it { should exist }
   its('owner') { should eq 'root' }
@@ -13,4 +7,10 @@ describe file('/etc/ssh/sshd_config') do
   its('mode') { should cmp '0644' }
   its('content') { should match("Managed by Ansible") }
   its('content') { should match("ansible-role-openssh") }
+end
+
+describe service('sshd') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
 end
