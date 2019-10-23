646c802045cc273102a54393196911587fb82704
Ahmad Wilson <ahwilson712@gmail.com>
2019-10-23 12:27:36 -0400

    refactor configuration rendering backup method and add sshd config validation

--- test/integration/service_config/default_test.rb
+++ test/integration/service_config/default_test.rb
@@ -6,13 +6,6 @@ describe service('sshd') do
   it { should be_running }
 end
 
-describe file('/etc/ssh/sshd_config.bak') do
-  it { should exist }
-  its('owner') { should eq 'root' }
-  its('group') { should eq 'root' }
-  its('mode') { should cmp '0644' }
-end
-
 describe file('/etc/ssh/sshd_config') do
   it { should exist }
   its('owner') { should eq 'root' }
